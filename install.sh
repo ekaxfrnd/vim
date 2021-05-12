5fdd54e1944a9ec71d5c113415bbd8b0eb658ae6
Ekagustimann <59228672+ekaxfrnd@users.noreply.github.com>
2021-05-12 13:34:23 +0800

    add auto install file

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# vim-plug installation
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+cp .vimrc ~/
